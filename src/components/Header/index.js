90f38cfb0a5811b65a4f96aa448677a41c911817
StephaneBusiere <stefbubu64@gmail.com>
2019-09-30 11:42:29 +0200

    WIP intégration home page responsive

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import logo from 'src/assets/images/borisdoyelogo_mini_white.png'
-import './styles.sass';
+import './header.sass';
 
 const Header = ({  }) => {
   
@@ -10,8 +10,9 @@ const Header = ({  }) => {
   return (
     <header className="header">
      <img className="logoWhite" src={logo} alt="Logo"/>
-     <h1>Agence</h1>
-     <h2>Boris Doye</h2>
+     <h1 className="AgencyName">Agence</h1>
+     <h2 className="ownerName" >Boris Doye</h2>
+     
     </header>
   );
 };
