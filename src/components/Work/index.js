93abdf7036bb204ff415d794f98ea92f3dd7023b
StephaneBusiere <stefbubu64@gmail.com>
2019-10-04 14:27:36 +0200

    ajout de texte au hover

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -11,23 +11,48 @@ import vietnam from 'src/assets/images/Vietnam.png'
 import './styles.sass';
 
 const Work= () => {
-  let sectionStyle={
+  let sectionStyle1={
     width: "100%",
     height: "600px",
-    
+   
     backgroundImage: `url(${bigPictureReal})`,
     backgroundSize: 'cover',
     overflow: 'hidden',
   }
+
+  let sectionStyle2={
+   
+    
+    width: "1000px", 
+    height: "600px",
+    margin: "0% 8% 0% 5%",
+   
+    backgroundImage: `url(${showreel})`,
+    backgroundSize: "",
+    display:"inline-block"
+
+    
+    
+    
+  }
     return <div>
-    <div className="mainPicture" style={sectionStyle}  >
+    <div className="mainPicture" style={sectionStyle1}  >
     <div className="presentationVideos" >NOS VIDEOS</div>
   
   </div>
-  <img className="showReel" src={showreel} alt="showreel"/>
+  <div className="falseMargin"></div>
+  <div className="videoPresentContainer">
+  <div className="showReelContainer">
+  <div className="showReel" style={sectionStyle2}  >
+  <div className="showReelContent"><p>Show reel</p></div>
+  </div>
+  </div>
+  
   <img className="queyras" src={queyras} alt="queyras"/>
   <img className="ski" src={ski} alt="ski"/>
   <img className="outdoormix" src={outdoormix} alt="outdoormix"/>
+  </div>
+  
   </div>
 };
 
