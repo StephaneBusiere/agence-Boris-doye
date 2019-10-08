672108c8de0974ea3589a37f2fb16d07f6e60068
StephaneBusiere <stefbubu64@gmail.com>
2019-10-08 14:54:06 +0200

    page equipement ok

--- src/components/Equipements/index.js
+++ src/components/Equipements/index.js
@@ -1,12 +1,166 @@
 import React from 'react';
 
+import avion from 'src/assets/images/avion.jpg'
+import phantom from 'src/assets/images/Phantom.jpg'
+import camera from 'src/assets/images/camera.jpg'
+import gimbal from 'src/assets/images/gimbal.jpg'
+import lens from 'src/assets/images/Lens.jpg'
+import filtres from 'src/assets/images/filtres.jpg'
+import slider from 'src/assets/images/slider.jpg'
+import micro from 'src/assets/images/micros.jpg'
+import goPro from 'src/assets/images/Go-pro.jpg'
+
+
+
+
 
 import './equipements.sass';
 
 const Equipements= () => {
 
-    return <div className="equipement">
-  Notre équipement !
+  let sectionStyle1={
+    width: "100%",
+    height: "700px",
+   
+    backgroundImage: `url(${avion})`,
+    backgroundSize: 'cover',
+    overflow: 'hidden',
+
+  }
+    return <div>
+    <div className="teamPicture" style={sectionStyle1}  >
+    <div className="presentationTeam" ><div className="teamtitle">Notre équipement</div>
+    
+    </div>
+     </div>
+
+     <div className="stuffTitleContainer">
+     <h2 className="stuffTitle">Equipement de pro pour vidéos de pro</h2>
+     <div className="separationLigne2"></div>
+     </div>
+     
+     <div className="stuffContainer">
+     
+     <div className="stuffContainer1">
+     
+     <img className="phantom" src={phantom} />
+     </div>
+     <div className="stuffText">
+     
+     
+     <h3 className="phantomTitle">Phamtom 4 pro</h3>
+     <p className="phantomTexte">Homologué avec 5 batteries…
+     Enregistre en 4k 60fps.</p>
+     </div>
+     
+     
+     <div className="stuffContainer1">
+     
+     <img className="phantom" src={camera} />
+     </div>
+     <div className="stuffText">
+     
+     
+     <h3 className="phantomTitle">Sony FS7</h3>
+     <p className="phantomTexte">Caméra Sony FS7, avec le capteur Exmor CMOS 4K Super 35 mm. Permet de filmer en 180 FPS ou encore en 4K 60P.</p>
+     </div>
+
+     </div>
+      
+     <div className="stuffContainer">
+     
+     <div className="stuffContainer1">
+     
+     <img className="phantom" src={gimbal} />
+     </div>
+     <div className="stuffText">
+     
+     
+     <h3 className="phantomTitle">Movi M10</h3>
+     <p className="phantomTexte">Le Movi M10 est un stabilisateur sur 3 axes pour caméra. Il est d’une grande flexibilité et très facile d’utilisation. Ultra léger, il permet d’embarquer des boîtiers type GH5 jusqu’à notre red EPIC W.
+     Il est fourni avec 6 batteries</p>
+     </div>
+     
+     
+     <div className="stuffContainer1">
+     
+     <img className="phantom" src={lens} />
+     </div>
+     <div className="stuffText">
+     
+     
+     <h3 className="phantomTitle">Lens DJI X7</h3>
+     <p className="phantomTexte">Kit de d’optique pour DJI X7 :
+     1 x Objectif DL-S 16 mm F2.8 ND ASPH
+     1 x Objectif DL 24 mm F2.8 
+     1 x Objectif DL 35 mm F2.8 
+     1 x Objectif DL 50 mm F2.8 
+     4 x Protection d’objectif
+     </p>
+     </div>
+
+     </div>
+     <div className="stuffContainer">
+     
+     <div className="stuffContainer1">
+     
+     <img className="phantom" src={filtres} />
+     </div>
+     <div className="stuffText">
+     
+     
+     <h3 className="phantomTitle">Filtres</h3>
+     <p className="phantomTexte">Différents types de filtres sont dispos ( circulaire, 4X5.5 ) , nous consulter pour la liste.
+     Ainsi que des zip box et matebox wooden.</p>
+     </div>
+     
+     
+     <div className="stuffContainer1">
+     
+     <img className="phantom" src={slider} />
+     </div>
+     <div className="stuffText">
+     
+     
+     <h3 className="phantomTitle">Slider Edelkrone</h3>
+     <p className="phantomTexte">Version de 1m avec déport, nous possédons l’option motorisée, qui permet de contrôler le temps et l’axe de la caméra.
+     Idéal pour un timelapse ou des plans studios très lents.
+     </p>
+     </div>
+
+     </div>
+     <div className="stuffContainer">
+     
+     <div className="stuffContainer1">
+     
+     <img className="phantom" src={micro} />
+     </div>
+     <div className="stuffText">
+     
+     
+     <h3 className="phantomTitle">Micros</h3>
+     <p className="phantomTexte">
+     Plusieurs type de solution micros disponible :
+     Micro HF, directionnel, enregistreur, micros gopro…
+     etc…</p>
+     </div>
+     
+     
+     <div className="stuffContainer1">
+     
+     <img className="phantom" src={goPro} />
+     </div>
+     <div className="stuffText">
+     
+     
+     <h3 className="phantomTitle">Gopro 7 Black edition</h3>
+     <p className="phantomTexte">
+     Plusieurs GOpro 7 black dispos, nous consulter pour les accessoires…
+     fixations, karma, batteries… </p>
+     </div>
+
+     </div>
+
 
   </div>;
   
--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 import teamDauphins from 'src/assets/images/teamdauphins.jpg'
 import borisBureau from 'src/assets/images/borisBureau.jpg'
-
+import cleo from 'src/assets/images/cleo.jpg'
+import hugo from 'src/assets/images/HugoClouzeau.jpg'
 
 import './team.sass';
 
@@ -18,7 +19,7 @@ const Team= () => {
   }
     return <div>
     <div className="teamPicture" style={sectionStyle1}  >
-    <div className="presentationTeam" ><h2>Notre équipe</h2>
+    <div className="presentationTeam" ><div className="teamtitle">Notre équipe</div>
     
     </div>
      </div>
@@ -41,21 +42,21 @@ const Team= () => {
      
      </div>
      </div>
+     
      <div className="teamContainer1">
      
      <div className="caphornPosterContainer">
      
-     <img className="borisPhoto" src={borisBureau} />
+     <img className="hugoPhoto" src={hugo } />
      </div>
      <div className="borisText">
      
-     <h2 className="borisTitle">Boris Doye</h2>
+     <h2 className="borisTitle">Hugo Clouzeau</h2>
      
-     <p className="borisPresentationTexte">Originaire de l’Isle sur Sorgue dans le Vaucluse, Boris est venu s’installer dans les Hautes-Alpes pour être au plus proche de ses passions. Car lorsqu’il n’est pas derrière sa caméra, Boris est dans un kayak, en train de dévaler les rivières de la Durance, du Guil, de l’Ubaye ; ou en train de survoler la vallée du Queyras sous sa voile de parapente.
-
-     Un aventurier dans l’âme à la recherche de sensations et de nouvelles expériences, mais avant tout un homme qui aime partager des moments intenses. Dans chaque aventure il y a des risques, mais Boris sait qu’il peut compter sur ses camarades pour veiller sur lui, et de cette confiance jaillit de grands moments et aussi de belles amitiés.
+     <p className="borisPresentationTexte">Hugo fait des films comme un artisan où chaque objet, chaque film est unique.
+     Hugo est investi dans tout le processus de création et de production. Aujourd'hui, en parallèle de travailler pour le secteur du documentaire il réalise ses propres projets et fusionne sa passion pour la production avec celle pour le kayak et l'aventure.
      
-     Par ailleurs notre aventurier ne se contente pas de vivre des moments extraordinaires, il les capture, prenant des risques parfois considérables dans le but de nous offrir un spectacle inoubliable. Lorsqu’on regarde ses films, on ne se rend pas toujours compte que le reporter se rend dans des endroits très reculés, loin de toute civilisation et souvent inaccessibles à n’importe quels secours. C’est donc au péril de sa vie que Boris enregistre ses images, et c’est ce qui rend ses films encore plus puissants.</p>
+     Le collectif vidéo « We are Hungry » est composé des kayakistes français : Stéphane Pion, Mathieu Coldebella, Louis Hétier, Guillaume Fievez et Hugo Clouzeau. Ils aiment mettre en image lerus virées kayak.</p>
      
      </div>
      
@@ -66,11 +67,11 @@ const Team= () => {
      
      <div className="caphornPosterContainer">
      
-     <img className="borisPhoto" src={borisBureau} />
+     <img className="cleoPhoto" src={cleo} />
      </div>
      <div className="borisText">
      
-     <h2 className="borisTitle">Boris Doye</h2>
+     <h2 className="borisTitle">Cléo Lhote</h2>
      
      <p className="borisPresentationTexte">Originaire de l’Isle sur Sorgue dans le Vaucluse, Boris est venu s’installer dans les Hautes-Alpes pour être au plus proche de ses passions. Car lorsqu’il n’est pas derrière sa caméra, Boris est dans un kayak, en train de dévaler les rivières de la Durance, du Guil, de l’Ubaye ; ou en train de survoler la vallée du Queyras sous sa voile de parapente.
 
