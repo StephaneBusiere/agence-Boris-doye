142bac6f1c928290fe34240d301f23e8c5604da0
StephaneBusiere <stefbubu64@gmail.com>
2019-10-09 21:01:17 +0200

    reduction taille video en responsive WIP

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -21,7 +21,7 @@ const Slider = (
       <div data-src={bigPictureReal2}>
       <p></p> </div>
      
-      <div className="sliderContainer"><iframe src="https://player.vimeo.com/video/198345088?loop=1&title=0&byline=0&portrait=0" width="1920" height="1080" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+      <div ><iframe className="sliderContainer" src="https://player.vimeo.com/video/198345088?loop=1&title=0&byline=0&portrait=0" width="1920" height="1080" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
       </div>
     </AwesomeSlider>
   );
