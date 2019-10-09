4ca9d92de308241be3c7e1b69887abb19b640d99
StephaneBusiere <stefbubu64@gmail.com>
2019-10-09 09:38:26 +0200

    tests video dans slider

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -6,13 +6,19 @@ import bigPictureReal from 'src/assets/images/alpesRealisateur.jpg'
 import showreel from 'src/assets/images/Showreel.jpg'
 import video from 'src/assets/images/fasboaVideo.mp4'
 //import video2 from 'https://www.youtube.com/watch?v=PEwa4Eehfpk'
+import { Player } from 'video-react';
+import AwsSliderStyles from './slider.scss';
 import './slider.scss';
-
+let video2= <Player>
+<source src={video} />
+</Player>
 const Slider = (
-    <AwesomeSlider>
+    <AwesomeSlider  cssModule={AwsSliderStyles}>
       <div data-src={bigPictureReal2}>
       <p></p> </div>
-      <div data-src={showreel}></div>
+      <div data-src={showreel}>
+      <p>Sometimes science is more art than science.</p>
+      </div>
       <div data-src={video} />
       
     </AwesomeSlider>
