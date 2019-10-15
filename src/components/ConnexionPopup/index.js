317d0b01fd18f4721b1f7b0057ad2d71abc36ae1
devTi44 <devti.oclock@gmail.com>
2019-10-15 15:13:22 +0200

    validation errors in process

--- src/components/ConnexionPopup/index.js
+++ src/components/ConnexionPopup/index.js
@@ -1,36 +1,95 @@
 import React, {Component} from "react";
 import { connect } from 'react-redux';
 import "./connexionPopup.scss";
+import axios from 'axios';
 
 class ConnexionPopup extends Component {
+	state = {
+		name: '',
+		email: '',
+		password: ''
+		}
+
+updateField = fieldName => event => {
+		this.setState({ [fieldName]: event.target.value });
+	}
+
+// répertorisation des erreurs:
+formErrors = [];
+
+
+	// sendRequest = (event) => {
+	// 	event.preventDefault();
+	// 	const promise = axios.post("../../data/users.js", {
+	// 		name: this.state.name,
+	// 		email: this.state.email,
+	// 		password: this.state.password
+	// 	});
+	// 	promise.then(this.handleResponse);
+	// 	}
+
+	// handleResponse = (response) => {
+	// 		this.setState({
+	// 		message: 'Identifiants invalides'
+	// 	})
+	// }
+
 	onClickAdd = () => {
 		container.classList.add("right-panel-active");
 	}
+
 	onClickRemove = () => {
 		container.classList.remove("right-panel-active");
 	}
+
 	render() {
 		console.log('we\'re in connexionpopup');
   return (
 		<div className="overAll">
-			<a className="close" href="/">X</a>
-		<div className="container" id="container">
+		<a className="close" href="/">X</a>
+		<div className="form-div-container" id="container">
 	    <div className="form-container sign-up-container">
-		    <form className="connexionForm" action="#">
+		    <form className="connexionForm" onSubmit={this.sendRequest} method="post">
 			    <h1 className="form__h1">Créez votre compte</h1>
-			      <input className="input" type="text" placeholder="Identifiant" />
-			      <input className="input" type="email" placeholder="Email" />
-			      <input className="input" type="password" placeholder="Mot de passe" />
-			    <button className="form__button create">Créer mon compte</button>
+						<input 
+						className="form__input"
+						type="text" 
+						name="name" 
+						placeholder="Identifiant" 
+						value={this.state.name}
+						onChange={this.updateField("name")}/>
+						<input 
+						className="form__input"
+						type="email" 
+						name="email" 
+						placeholder="Email" 
+						value={this.state.email}
+						onChange={this.updateField("email")}/>
+						<input 
+						className="form__input"
+						type="password" 
+						name="password" 
+						placeholder="Mot de passe"
+						value={this.state.password}
+						onChange={this.updateField("password")} />
+			    <button className="form__button create" type="submit">Créer mon compte</button>
 		    </form>
 	    </div>
 	    <div className="form-container sign-in-container">
-		    <form className="connexionForm" action="#">
+		    <form className="connexionForm" method="post">
 			    <h1 className="form__h1">Connectez-vous</h1>
-			      <input className="input" type="email" placeholder="Email" />
-			      <input className="input" type="password" placeholder="Mot de passe" />
+						<input 
+						className="form__input"
+						type="email" 
+						placeholder="Email" 
+						name="emailConnexion"/>
+						<input 
+						className="form__input"
+						type="password" 
+						placeholder="Mot de passe" 
+						name="pwdConnexion"/>
 			      <a className="lost" href="#">Mot de passe perdu ?</a>
-			    <button className="form__button connect">Connexion</button>
+			    <button className="form__button connect" type="submit">Connexion</button>
 		    </form>
 	    </div>
 	    <div className="overlay-container">
