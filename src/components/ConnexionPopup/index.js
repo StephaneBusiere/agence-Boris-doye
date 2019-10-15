d45d3f4412675ca4d01d4b3e0d0f1a2e586cb057
devTi44 <devti.oclock@gmail.com>
2019-10-15 14:14:27 +0200

    add className

--- src/components/ConnexionPopup/index.js
+++ src/components/ConnexionPopup/index.js
@@ -13,37 +13,37 @@ class ConnexionPopup extends Component {
 		console.log('we\'re in connexionpopup');
   return (
 		<div className="overAll">
-			<a class="close" href="/">X</a>
+			<a className="close" href="/">X</a>
 		<div className="container" id="container">
 	    <div className="form-container sign-up-container">
-		    <form action="#">
-			    <h1>Créez votre compte</h1>
-			      <input type="text" placeholder="Identifiant" />
-			      <input type="email" placeholder="Email" />
-			      <input type="password" placeholder="Mot de passe" />
-			    <button className="create">Créer mon compte</button>
+		    <form className="connexionForm" action="#">
+			    <h1 className="form__h1">Créez votre compte</h1>
+			      <input className="input" type="text" placeholder="Identifiant" />
+			      <input className="input" type="email" placeholder="Email" />
+			      <input className="input" type="password" placeholder="Mot de passe" />
+			    <button className="form__button create">Créer mon compte</button>
 		    </form>
 	    </div>
 	    <div className="form-container sign-in-container">
-		    <form action="#">
-			    <h1>Connectez-vous</h1>
-			      <input type="email" placeholder="Email" />
-			      <input type="password" placeholder="Mot de passe" />
-			      <a href="#">Mot de passe perdu ?</a>
-			    <button className="connect">Connexion</button>
+		    <form className="connexionForm" action="#">
+			    <h1 className="form__h1">Connectez-vous</h1>
+			      <input className="input" type="email" placeholder="Email" />
+			      <input className="input" type="password" placeholder="Mot de passe" />
+			      <a className="lost" href="#">Mot de passe perdu ?</a>
+			    <button className="form__button connect">Connexion</button>
 		    </form>
 	    </div>
 	    <div className="overlay-container">
 		<div className="overlay">
 			<div className="overlay-panel overlay-left">
-				<h1>Quel plaisir de vous revoir !</h1>
-				<p>Merci de vous identifier.</p>
-				<button className="ghost" id="signIn" onClick={this.onClickRemove}>Connexion</button>
+				<h1 className="form__h1">Quel plaisir de vous revoir !</h1>
+				<p className="form__p">Merci de vous identifier.</p>
+				<button className="form__button ghost" id="signIn" onClick={this.onClickRemove}>Connexion</button>
 			</div>
 			<div className="overlay-panel overlay-right">
-				<h1>Bienvenu·e !</h1>
-				<p>Enregistrez-vous...</p>
-				<button className="ghost" id="signUp" onClick={this.onClickAdd}>Créer mon compte</button>
+				<h1 className="form__h1">Bienvenu·e !</h1>
+				<p className="form__p">Enregistrez-vous...</p>
+				<button className="form__button ghost" id="signUp" onClick={this.onClickAdd}>Créer mon compte</button>
 			</div>
 		</div>
 	</div>
