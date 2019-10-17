74fd40fc658d3601383b1e653ca8c967c4a32a23
StephaneBusiere <stefbubu64@gmail.com>
2019-10-17 11:59:11 +0200

    ajout formulaire contact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -7,13 +7,43 @@ const Contact= () => {
 
     return <div>
         <div className="form_contact">
-        <form>
-        <h1 className="contact__h1">Contactez nous</h1>
-            <input className="contact-input" type="text"/>
-            <input type="text"/>
-            <button type="submit">Bye</button>
+        <form
+        className="contact2-form validate-form">
+					<span className="contact2-form-title">
+						Contactez nous
+					</span>
+
+					<div className="wrap-input2 validate-input" data-validate="Name is required">
+						<input class="input2" type="text" name="name"/>
+						<span class="focus-input2" data-placeholder="NOM/PRENOM"></span>
+					</div>
+
+					<div class="wrap-input2 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+						<input class="input2" type="text" name="email"/>
+						<span class="focus-input2" data-placeholder="EMAIL"></span>
+					</div>
+
+					<div class="wrap-input2 validate-input" data-validate = "Message is required">
+						<textarea class="input2" name="message"></textarea>
+						<span class="focus-input2" data-placeholder="MESSAGE"></span>
+					</div>
+
+					<div class="container-contact2-form-btn">
+						<div class="wrap-contact2-form-btn">
+							<div class="contact2-form-bgbtn"></div>
+							<button class="contact2-form-btn">
+								Envoyez votre message
+							</button>
+						</div>
+					</div>
         </form>
+        <div className="emailsContainer">
+        <a className='mail' href="boris.doye.films@gmail.com">boris.doye.films@gmail.com</a><br />
+        <a className='tel' href="tel:+33609614477">+33609614477</a>
         </div>
+        
+        </div>
+        
         <div class="wrapper">
             <div class="snow layer1 a"></div>
             <div class="snow layer1"></div> 
