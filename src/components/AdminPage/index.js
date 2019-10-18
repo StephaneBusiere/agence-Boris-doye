a78eb98521b521d36a49af1db4a7b0dab025cdf1
devTi44 <devti.oclock@gmail.com>
2019-10-18 15:39:52 +0200

    add adminPage

--- src/components/AdminPage/index.js
+++ src/components/AdminPage/index.js
@@ -1,16 +1,73 @@
 import React from 'react';
-
+import queyras from 'src/assets/images/Queyras.jpg'
 
 import './adminPage.sass';
 
 const AdminPage= () => {
 
-    return <div >
-  Bienvenue Admin ! 
+  let sectionStyle1
+  if (window.innerWidth<475) {
+    sectionStyle1={ 
+      width: "100%",
+      height: "100%",
+      backgroundImage: `url(${queyras})`,
+      backgroundSize: 'cover',
+      overflow: 'hidden',
+    }
+   
+    } else {
+      sectionStyle1={ 
+        width: "100%",
+        height: "60em",
+       
+        backgroundImage: `url(${queyras})`,
+        backgroundSize: 'cover',
+        overflow: 'hidden',
+      }
+    }
   
-  </div>;
+    return <div className="login">
+      <div className="avionContainer">
+        <div style={sectionStyle1}  >
+        <div ClassName="accountTitle">PAGE ADMIN</div>
+        </div>
+      </div>
   
-}; 
+    <div className="login-info">  
+    <div className="yours">
+    <table>
+    <thead>
+        <tr>
+            <th colspan="2">Les vidéos</th>
+            <th colspan="2">Les articles</th>
+            <th colspan="2">Les images</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Nom1</td>
+            <td>Nom2</td>
+            <td>Nom1</td>
+            <td>Nom2</td>
+            <td>Nom1</td>
+            <td>Nom2</td>
+        </tr>
+        <tr>
+            <td>Lien1</td>
+            <td>Lien2</td>
+            <td>Lien1</td>
+            <td>Lien2</td>
+            <td>Lien1</td>
+            <td>Lien2</td>
+        </tr>
+    </tbody>
+  </table>
+
+    </div> 
+    </div>;
+    </div>
+  };
+
 
 
 
