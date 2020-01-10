e225dfa882fffbd7e35b3eef78fb7ba646e1093d
StephaneBusiere <stefbubu64@gmail.com>
2020-01-10 16:16:58 +0100

    finalisation section présentation

--- src/components/Home/AgencyPresentation/index.js
+++ src/components/Home/AgencyPresentation/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import iconDrone from 'src/assets/images/iconDrone.svg'
+
 import iconDroneNew from 'src/assets/images/iconDroneFinal.svg'
 import borisPhotoCarre from 'src/assets/images/photoBorisCarre.jpg'
 import borisBateau from 'src/assets/images/BorisBateau.jpg'
@@ -17,11 +17,11 @@ const AgencyPresentation = (
     
     <div className="textPresentation1">
     <img className="borisPhotocarre" src={borisPhotoCarre} alt="borisPhotocarre"/>
-    <h2 className="quitexte"> Agence</h2>
+    <h2 className="quitexte"> Vidéos</h2>
 
     <div className="smallTitrePresentation1">
     
-    <h3 className="videoTitle" >Vidéo</h3></div>
+    <h3 className="videoTitle" >Proféssionnelles</h3></div>
     <p className="videoTitleText" >Avec une touche artistique et créative, Boris Doye Films vous offre un service global professionnel de conception et de réalisation de films au coeur des Hautes-Alpes.
 
     Réalisateur, mais aussi concepteur du scénario de votre vidéo, une idée, une envie ? Nous vous aiderons à réaliser votre projet, du scénario au montage de votre film. Nous maîtrisons l’image et nous savons la rendre esthétique. À l’aide de nombreuses caméras, drones, stabilisateurs, nous avons la capacité de filmer partout et de monter vos images sur une station de montage professionnel 4K.</p>
@@ -30,23 +30,23 @@ const AgencyPresentation = (
     
     <div className="textPresentation2">
     <img className="borisBateau" src={borisBateau} alt="borisBateau"/>
-    <h2 className="sommestexte"> De production</h2>
+    <h2 className="sommestexte"> Conseils</h2>
 
     <div className="smallTitrePresentation2">
     
     
-    <h3 className="conseilsTitle">Conseils</h3></div>
+    <h3 className="conseilsTitle">Personnalisés</h3></div>
     <p className="conseilsTitleText">Envie d’originalité ? L’agence vous propose de l’animation 3D, des time-laps, du slow-motion, de stop-motion, de la prise de vue aérienne en drone… Avec le grand réseau professionnel qui nous entoure, nous réalisons tout support audiovisuel.Nous amorçons ensemble une réflexion cohérente pour une production réalisable et conforme à vos attentes.
     </p>
     </div>
     
     <div className="textPresentation3">
     <img className="borisDrone" src={borisDrone} alt="borisDrone"/>
-    <h2 className="noustexte">Audiovisuelle</h2>
+    <h2 className="noustexte">Drones</h2>
     <div className="smallTitrePresentation3">
     
    
-    <h3 className="conseilsTitle">Drones</h3> </div>
+    <h3 className="conseilsTitle">Spectaculaires</h3> </div>
     <p className="dronesTitleText">Envie d’originalité ? L’agence vous propose de l’animation 3D, des time-laps, du slow-motion, de stop-motion, de la prise de vue aérienne en drone… Avec le grand réseau professionnel qui nous entoure, nous réalisons tout support audiovisuel.Tous les formats sont réalisables. De la promotion institutionnelle, format court corporate pour les réseaux sociaux, au films d’entreprise, prise de vues en drone reportages TV mais l’agence sait aussi créer de fabuleux documentaires télévisés.</p>
     </div>
       </div>
